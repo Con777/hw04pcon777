ad3b88c6341efc8af618a1c50e01fa125aa3c093
Connor HaggertyCon777 <ch8384a@student.american.edu>
2021-10-13 12:36:00 -0400

    function edits. still struggling

--- R/my_df.R
+++ R/my_df.R
@@ -1,41 +1,17 @@
-my_df <- function(x, y, z, n) {
-  # if(n<0){
-  #   stop("n must be positive!")
-  # }
-  # else{
-  # df <- data.frame(x = rep.int(x, n), y = rep.int(y, n),
-  #               z = rep.int(z, n), n = (1:n))
-  # assign("df",df,envir = .GlobalEnv)
-  # View(df)
-  # plot(df$n, )
-#   }
-# }
-
-my_df <- function(x) {
-
-    df <- data.frame(x)
-    assign("df",df,envir = .GlobalEnv)
-    my_seq_df <- myseq_n(x)
-    View(df)
-    ggplot(x = df$n, y = my_seq_df)
-}
-
 my_df2 <- function(df){
   stopifnot(is.data.frame(df))
   df$output <- 0
 for (i in seq_along(nrow(df)))
-  df2 <- as.numeric(unlist(df))
-  df2 <- split(df2, ceiling((df)))
-  df$output[[i]] <- myseq_n(df[[i]], df$n[[i]])
+  df2 <- df[1:(length(df)-2)]
+  df2 <- as.numeric(unlist(df2))
+  df2 <- split(df2, f = 1:nrow(df))
   n <- as.numeric(df$n)
+  n <- split(n, f = nrow(df))
+  df$output[[i]] <- myseq_n(x = df2[[i]], df$n[[i]])
   df
 }
-# row.wise
-# for loop converting entr
 
-
-
-my_data <- tibble::tribble(
+@my_data <- tibble::tribble(
   ~x, ~y, ~z, ~n,
   4, 4, 3, 2,
   4, 4, 3, 3,
